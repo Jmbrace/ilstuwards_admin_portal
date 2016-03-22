4c7ca9f9ec4833402b414663eb3ca57d91ddfadd
Adrian Gracia <Shyscope@gmail.com>
2016-03-22 16:19:53 -0500

    Mail gun added. Issue with going into the user portal. App keeps redirecting to login page that does not exist.

--- admin/server/modules/send-invitation.js
+++ admin/server/modules/send-invitation.js
@@ -28,3 +28,4 @@ let _sendInvitation = ( email, content ) => {
 };
 
 Modules.server.sendInvitation = invitation;
+
--- admin/server/modules/startup.js
+++ admin/server/modules/startup.js
@@ -2,18 +2,18 @@ let startup = () => {
   _setEnvironmentVariables();
   _setBrowserPolicies();
   _generateAccounts();
-  _setAdmins();
+  // _setAdmins();
 };
 
 let _setEnvironmentVariables = () => {
   let settings = Meteor.settings.private;
-  process.env.MAIL_URL = settings.MAIL_URL;
+  process.env.MAIL_URL = "smtp://postmaster%40sandbox89c658f7407e4beba0ebdfc77143c297.mailgun.org:f5a78530906b0c0ff90291187863d297@smtp.mailgun.org:587";
 };
 
 let _setBrowserPolicies = () => {};
 
 let _generateAccounts = () => Modules.server.generateAccounts();
 
-let _setAdmins = () => Modules.server.setAdmins();
+// let _setAdmins = () => Modules.server.setAdmins();
 
 Modules.server.startup = startup;
