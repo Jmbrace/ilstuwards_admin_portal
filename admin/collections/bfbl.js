f1a67971f90d3031a09b2e4698ccc6ddc4715d50
Adrian Gracia <Shyscope@gmail.com>
2016-04-24 19:20:26 -0500

    bfbl collection created with schema attached.

---
+++ admin/collections/bfbl.js
@@ -0,0 +1,45 @@
+BFBL = new Meteor.Collection( 'bfbl' );
+
+BFBLSchema = new SimpleSchema({
+  business: { type: String, defaultValue: null },
+  name: { type: String , defaultValue: null},
+  phone: { type: String , defaultValue: null }, 
+  email: { type: String, defaultValue: null },
+  address: { type: String, defaultValue: null },
+  city: { type: String, defaultValue: null },
+  state: { type: String, defaultValue: null },
+  zip: { type: String, defaultValue: null },
+  county: { type: String, defaultValue: null },
+  description: { type: String, defaultValue: null },
+  business_phone: { type: String, defaultValue: null },
+  business_email: { type: String, defaultValue: null },
+  website: { type: String, defaultValue: null },
+  facebook: { type: String, defaultValue: null },
+  hours_of_operation: { type: String, defaultValue: null}
+  lat: { type: Number, defaultValue:null},
+  long: { type: Number, defaultValue: null}
+});
+
+BFBL.attachSchema( BFBL.schema );
+
+
+// BFBL.insert( { example of how to insert
+
+//   business: 'Acbees Apiaries',
+//   name: 'Arvin Pierce',
+//   address: '16813 Lowder Rd',
+//   city: 'Waverly',
+//   state: 'IL',
+//   zip: '62692',
+//   county: '',
+//   description: '',
+//   business_phone: '',
+//   business_email: '',
+//   website: '',
+//   facebook: '',
+//   hours_of_operation: ''
+ 
+// }, { validate: false, filter: false }, 
+// function(error, result){
+// 	//error
+// });
